da4dde1125b8196de652a4856532c6c34821604d
andersbandt <andersbandt@yahoo.com>
2018-11-13 19:00:58 -0600

    wiring_verification

---
+++ enlight_ez/enlight_ez.ino
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+int count = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+   digitalWrite(5,HIGH);
+
+   digitalWrite(6,count & 1);
+   digitalWrite(7,count & 2);
+   digitalWrite(8, count & 4);
+   digitalWrite(9, count & 8);
+   digitalWrite(10,count & 16);
+   digitalWrite(11,count & 32);
+   digitalWrite(12, count & 64);
+   digitalWrite(13, count & 128);
+
+
+    delay(800);
+
+    count++;
+  
+}
